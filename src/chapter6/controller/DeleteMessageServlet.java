981c6f3727267769394dd172d1c730efda19443f
taguchi.keita <taguchi.keita@alhinc.jp>
2025-07-30 09:26:34 +0900

    簡易Twitter仕様追加（つぶやきの編集）

---
+++ src/chapter6/controller/DeleteMessageServlet.java
@@ -0,0 +1,53 @@
+package chapter6.controller;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import chapter6.logging.InitApplication;
+import chapter6.service.MessageService;
+
+@WebServlet(urlPatterns = { "/deleteMessage" })
+public class DeleteMessageServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	* ロガーインスタンスの生成
+	*/
+	Logger log = Logger.getLogger("twitter");
+
+	/**
+	* デフォルトコンストラクタ
+	* アプリケーションの初期化を実施する。
+	*/
+	public DeleteMessageServlet() {
+		InitApplication application = InitApplication.getInstance();
+		application.init();
+
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		//パラメータを取得
+		String messageId = request.getParameter("messageId");
+		//更新時は返り値が無い
+		new MessageService().delete(messageId);
+
+		//delete処理が終わり次第、リダイレクト
+		response.sendRedirect("./");
+	}
+
+}
---
+++ src/chapter6/controller/EditServlet.java
@@ -0,0 +1,149 @@
+package chapter6.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.lang.StringUtils;
+
+import chapter6.beans.Message;
+import chapter6.logging.InitApplication;
+import chapter6.service.MessageService;
+
+@WebServlet(urlPatterns = { "/edit" })
+public class EditServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	* ロガーインスタンスの生成
+	*/
+	Logger log = Logger.getLogger("twitter");
+
+	/**
+	* デフォルトコンストラクタ
+	* アプリケーションの初期化を実施する。
+	*/
+	public EditServlet() {
+		InitApplication application = InitApplication.getInstance();
+		application.init();
+
+	}
+
+	/*画面表示*/
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		//requestからパラメータを取得、intに型変換
+		//String strMessageId = request.getParameter("messageId");
+		//int messageId = Integer.parseInt(strMessageId);
+
+		String strMessageId = request.getParameter("messageId");
+		HttpSession session = request.getSession();
+		List<String> errorMessages = new ArrayList<>();
+
+	    try {
+	        // 空 or 空白 or null チェック
+	        if (strMessageId == null || StringUtils.isBlank(strMessageId)) {
+	            throw new NumberFormatException();
+	        }
+
+	        // 数値に変換できるか
+	        int messageId = Integer.parseInt(strMessageId);
+
+	        // メッセージ取得
+	        Message message = new MessageService().select(messageId);
+	        if (message == null) {
+	            // 存在しないID
+	            errorMessages.add("不正なパラメータが入力されました");
+	            session.setAttribute("errorMessages", errorMessages);
+	            response.sendRedirect("./"); // トップに戻る
+	            return;
+	        }
+
+	        // 正常時 → 編集画面へ
+	        //jspのmessageという名前に、DBで参照した値をセットする
+	        request.setAttribute("message", message);
+	        request.getRequestDispatcher("edit.jsp").forward(request, response);
+
+	    } catch (NumberFormatException e) {
+	        // 数字以外、またはnull/空白
+	        errorMessages.add("不正なパラメータが入力されました");
+	        session.setAttribute("errorMessages", errorMessages);
+	        response.sendRedirect("./"); // トップに戻る
+	    }
+	}
+
+
+	/*編集画面　編集ボタン押下後*/
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+
+		log.info(new Object() {
+		}.getClass().getEnclosingClass().getName() +
+				" : " + new Object() {
+				}.getClass().getEnclosingMethod().getName());
+
+		//ただの文字として入力される→画面にはJava側の型とかは無いため
+		String messageIdStr = request.getParameter("messageId");
+
+		//空文字を型変換しようとしています、というエラーになる
+		int messageid = Integer.parseInt(messageIdStr);
+
+		String messageText = request.getParameter("text");
+
+		// バリデーションの実行
+		List<String> errorMessages = new ArrayList<>();
+
+		if (!isValid(messageText, errorMessages)) {
+			// バリデーションエラーがあった場合、エラーメッセージをセット
+			request.setAttribute("errorMessages", errorMessages);
+			Message message = new Message();
+		    message.setId(messageid);
+		    message.setText(messageText);
+		    request.setAttribute("message", message);
+
+			// エラーメッセージを再度表示し、編集画面に戻る
+			request.getRequestDispatcher("edit.jsp").forward(request, response);
+			return; // 更新処理は行わない
+		}
+
+			new MessageService().update(messageText, messageid); // メッセージの更新
+			// 更新後はホーム画面にリダイレクト
+			response.sendRedirect("./");
+
+	}
+
+	private boolean isValid(String messageText, List<String> errorMessages) {
+
+	    log.info(new Object() {}.getClass().getEnclosingClass().getName() +
+	            " : " + new Object() {}.getClass().getEnclosingMethod().getName());
+
+	    // 空のメッセージ
+	    if (StringUtils.isBlank(messageText)) {
+	        errorMessages.add("メッセージを入力してください");
+	    }
+	    // 140文字を超えている場合
+	    else if (messageText.length() > 140) {
+	        errorMessages.add("140文字以下で入力してください");
+	    }
+
+	    // バリデーションが通らない場合はエラーメッセージリストが返る
+	    return errorMessages.isEmpty();
+	}
+}
